dce32341302769a90d78add92b995a5d567e2971
Valente <fernandovalentedev@gmail.com>
2020-08-10 14:09:23 -0300

    binary gap in python

--- binarygap.py
+++ binarygap.py
@@ -1,6 +1,17 @@
 # Find longest sequence of zeros in binary representation of an integer.
 
+def count_binary_gap():
+    num = int(input("Enter an integer "))
+    bin_num = bin(num)[2:]
+
+    print(('{} in binary is {}').format(num, bin_num))
+
+    gap = 0
+    for n in bin_num.split('1'):
+        if n:
+            gap += 1
+            print(('Gap {}. Length {}').format(gap, len(n)))
 
 
 if __name__ == '__main__':
-    pass
+    count_binary_gap()
