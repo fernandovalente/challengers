28dbbf570dcd8b6f85ee067314a0d44bbe41b8dc
Valente <fernandovalentedev@gmail.com>
2020-08-13 13:46:37 -0300

    add references

--- binarygap.py
+++ binarygap.py
@@ -1,6 +1,9 @@
 import unittest
 
-# Find longest sequence of zeros in binary representation of an integer.
+'''
+Find longest sequence of zeros in binary representation of an integer.
+https://app.codility.com/programmers/lessons/1-iterations/
+'''
 
 def return_binary_num(num):
     return bin(num)[2:]
---
+++ cyclicrotation.py
@@ -0,0 +1,18 @@
+import unittest
+
+'''
+An array A consisting of N integers is given. Rotation of the array means that
+each element is shifted right by one index, and the last element of the array
+is moved to the first place. For example, the rotation of array
+A = [3, 8, 9, 7, 6] is [6, 3, 8, 9, 7]
+(elements are shifted right by one index and 6 is moved to the first place).
+https://app.codility.com/programmers/lessons/2-arrays/cyclic_rotation/
+'''
+
+
+class CyclicTest(unittest.TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
